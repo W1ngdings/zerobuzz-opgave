0766bbbb2da6bf339261ba87501ad61c2e32d3bd
linegj <linekbv@gmail.com>
2025-02-28 12:04:54 +0100

    kurv commit

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -60,23 +60,32 @@ function addtocart(a){
     displaycart();
 }
 
+function delElement(a){
+    cart.splice(a,1);
+    displaycart();
+}
+
 function displaycart(a){
-    let j = 0;
+    let j = 0, total=0;
+    document.getElementById('count').innerHTML = cart.length;
     if (cart.length ==0){
         document.getElementById('cartItem').innerHTML = "Your cart is empty";
+        document.getElementById('total').innerHTML = "€ "+0+"0.00";
     }
     else {
-        document.getElementById("cartItem").innerHTML = cart.map((item) => 
+        document.getElementById("cartItem").innerHTML = cart.map((items) => 
             {
-                var {image, title,price} = item;
+                var {image, title,price} = items;
+                total=total+price;
+                document.getElementById('total').innerHTML = "€ "+total+".00";
                 return (
                     `<section class='cart-item'>
                     <section class='row-img'>
-                        <img class='rowing' src=${image}></img>
+                        <img class='rowimg' src=${image}>
                     </section>
                     <p style = 'font-size:12px;'>${title}</p>
-                    <h2 style = 'font-size:15px;'>${price}.00 €</h2>` +
-                    "<i class = 'fa-solid fa-trash' onclick='delElement(" + (j++)+")'></i></section>"
+                    <h2 style = 'font-size:15px;'>${price}.00</h2>` +
+                    "<i class = 'fa-solid fa-trash' onclick='delElement("+(j++)+")'></i></section>"
                 );
         
         }).join('');
