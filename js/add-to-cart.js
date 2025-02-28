2231ce9634b3839a544ea43e1f3d25c8bd28b88f
linegj <linekbv@gmail.com>
2025-02-28 10:59:37 +0100

    kurv commit 3

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -15,7 +15,7 @@ const product = [
     },
     {
         id:2,
-        image: "images/cherry-buzz.oel.png",
+        image: "images/cherry-buzz-oel.png",
         title: "Cherry Buzz",
         price: 3,
     },
@@ -27,7 +27,7 @@ const product = [
     },
     {
         id:4,
-        image: "images/kortaeske.png",
+        image: "images/kortaeske-mockups.png",
         title: "Ice Breakers",
         price: 10,
     }
