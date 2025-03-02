901ee0eb4dcc6ebe6a27ec44cff7ffc304e3144c
PernilleRefsgaard <Refsgaardpernille@gmail.com>
2025-03-02 13:27:02 +0100

    flere forsøg

--- js/infografik.js
+++ js/infografik.js
@@ -5,13 +5,36 @@ document.addEventListener("DOMContentLoaded", function() {
     
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
-            console.log("Observer registrerer:", entry.target); 
+           console.log("Observer registrerer:", entry.target); 
             if (entry.isIntersecting) {
                 console.log("Element er synligt:", entry.target);
                 entry.target.classList.add("show");
-            }
+           }
         });
     }, { threshold: 1 });
 
     ingredients.forEach(ingredient => observer.observe(ingredient));
 });
+
+
+
+
+
+
+
+
+//document.addEventListener("DOMContentLoaded", function() {
+   // const ingredients = document.querySelectorAll("[class^='ingredient']");
+    
+    //const observer = new IntersectionObserver(entries => {
+      //  entries.forEach(entry => {
+         //  console.log("Observer registrerer:", entry.target); 
+           // if (entry.isIntersecting) {
+              //console.log("Element er synligt:", entry.target);
+               // entry.target.classList.add("show");
+          // }
+        //});
+    //}, { threshold: 1 });
+
+  //  ingredients.forEach(ingredient => observer.observe(ingredient));
+//});
