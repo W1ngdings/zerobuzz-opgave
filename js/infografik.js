22be6a947283d6396f301a4c922fdecb66b2d680
PernilleRefsgaard <Refsgaardpernille@gmail.com>
2025-03-02 14:41:58 +0100

    .

--- js/infografik.js
+++ js/infografik.js
@@ -1,5 +1,4 @@
 "use-strict";
-
 document.addEventListener("DOMContentLoaded", function() {
     const ingredients = document.querySelectorAll("[class^='ingredient']");
     
@@ -20,24 +19,3 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-
-
-
-
-
-
-//document.addEventListener("DOMContentLoaded", function() {
-   // const ingredients = document.querySelectorAll("[class^='ingredient']");
-    
-    //const observer = new IntersectionObserver(entries => {
-      //  entries.forEach(entry => {
-         //  console.log("Observer registrerer:", entry.target); 
-           // if (entry.isIntersecting) {
-              //console.log("Element er synligt:", entry.target);
-               // entry.target.classList.add("show");
-          // }
-        //});
-    //}, { threshold: 1 });
-
-  //  ingredients.forEach(ingredient => observer.observe(ingredient));
-//});
